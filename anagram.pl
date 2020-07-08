d99dacfdedc32a82212b71a6596550da73257f0b
Antonia <konto_326@tu-berlin.de>
2020-07-08 12:10:52 +0200

    Initial commit

---
+++ anagram.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+@single_vowels = () # a, e, i, o, u, ä
+@long_vowels_diphthongs = () # aa, ai, ah, äh, au
+@double_consonants = () # bb, cc, dd
+@single_consonants = () # b, c, d, sch, ch, ck
+@affricates = () # tz, pf, fr
+@
