b3818ca88fda74f8f202415be96f167346ae4581
Antonia <konto_326@tu-berlin.de>
2020-07-10 13:36:52 +0200

    Read subsyllabic units from file and group them according to their distributional properties

--- anagram.pl
+++ anagram.pl
@@ -3,9 +3,43 @@
 use strict;
 use warnings;
 
-@single_vowels = () # a, e, i, o, u, ä
-@long_vowels_diphthongs = () # aa, ai, ah, äh, au
-@double_consonants = () # bb, cc, dd
-@single_consonants = () # b, c, d, sch, ch, ck
-@affricates = () # tz, pf, fr
-@
+#my @single_vowels = (); # a, e, i, o, u, ä
+#my @vowel_clusters = (); # aa, ai, ah, äh, au
+#my @cluster_notinitial = (); # tz, ck
+my @vowels = ();
+my @double_consonants = (); # bb, cc, dd, ck, tz
+my @single_consonants = (); # b, c, d, sch, ch 
+my @cluster_ltf = (); # bl, schl, st, pfl, pf
+my @cluster_rmn = (); # fr, spr, chr, schm, schn, str
+
+open(DATA, "<$ARGV[0]") or die "Couldn't open file!";
+
+while(<DATA>) {
+    $_ =~ s/\n//;
+    if (/^(a|e|i|o|u|ä|ö|ü)/) {
+	push(@vowels, $_);
+    }
+    elsif (/([a-z])\1/ or /(ck|tz)/) {
+	push(@double_consonants, $_);
+    }  
+    elsif (/.+(l|t|f)\z/) {
+	push(@cluster_ltf, $_);
+    }  
+    elsif (/.+(r|m|n)\z/) {
+	push(@cluster_rmn, $_);
+    }  
+    #elsif (/.+(z|k)\z/) {
+    #	push(@cluster_notinitial, $_);
+    #}  
+    else {
+	push(@single_consonants, $_);
+    }  
+}
+
+print "vowels : @vowels\n";
+print "double consonants and noninital consonant clusters: @double_consonants\n";
+print "clusters ending with l, t, or f : @cluster_ltf\n";
+print "clusters ending with r, m, or n : @cluster_rmn\n";
+#print "noninitial clusters ending with z or k : @cluster_notinitial\n";
+print "single consonants : @single_consonants\n";
+close(DATA) or die "Could not close file properly!";
