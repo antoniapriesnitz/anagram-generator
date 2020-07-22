96849c48f0528094d60231f5233fa50a11de6085
Antonia <konto_326@tu-berlin.de>
2020-07-22 22:32:10 +0200

    Everything works now. Further restrictions needed as some of the generated anagrams are wrong in terms of graphotactics.

--- anagram.pl
+++ anagram.pl
@@ -9,8 +9,7 @@ my $length = length($word);
 print "Input word: $word, length = $length\n";
 
 my %all = ();
-my @vowel_clusters = (); # solitary or clusters
-my @single_consonants = (); # b, c, d, sch, ch (do we need this? same as line below)
+my @vowel_clusters = (); # single or clusters
 my @onset_coda_ambisyllabic_clusters = (); # bl, schl, st, pfl, pf 
 my @not_coda_clusters = (); # fr, spr, chr, schm, schn, str 
 my @not_onset_clusters = (); # rbst, scht, lm, lst, fst, rfst, rsch, sst, chst, ckst, gst, tzt, schst, chst, kst, mmst, mst, nnst, nst, pst, vn,  rscht, rm, ffst, ppst, bbst, rrst, rst, rn, lln, llst, ml, rschst, rft, cht 
@@ -49,9 +48,7 @@ close(DATA) or die "Could not close file properly!";
 # Counts the occurences of all letters of a word
 # Returns a hash of the form letter: occurences
 sub letter_count {
-    #print "input = @_\n";
     my $w = shift;
-    #print "w in letter_count: $w\n";
     my @letters = split(//, $w);
     my %letter_count;
     for(@letters){
@@ -138,59 +135,56 @@ sub calc_syllables {
     #$syllables = $single_vowels + $y_count;
     #}
     #else {
-    my $syllable = $single_vowels + $y_count;
-    if ($syllable > 0) {
-        $syllable += 1; # first and last element in generated anagram will be syllable start marker "|"
+    my $syllables = $single_vowels + $y_count;
+    if ($syllables > 0) {
+        $syllables += 1; # first and last element in generated anagram will be syllable start marker "|"
     }
-    return $syllable;
+    return $syllables;
 }
 
 # Takes a cluster of letters and hash of letter: occurrences pairs as parameters
 # Subtracts 1 from each letter that is used by the cluster
 # Returns the changed hash
 sub countdown_letters {
-    #my ($cluster, $l_instances_ref, $c_instances_ref) = @_;
     my ($cluster, $l_instances_ref) = @_;
     my %l_instances = %{$l_instances_ref};
-    #my %c_instances = %{$c_instances_ref};
+    #print "cluster in countdown_letters:\t $cluster\n";
+    #print "l_instances in countdown_letters:\t";
+    #print "$_: $l_instances{$_}\t" for keys(%l_instances);
+    #print "\n";
     my %l_instances_modified = %l_instances;
-    #my %c_instances_modified = %c_instances;
     my @letters = split(//, $cluster);
     my $bool = 1;
     for(@letters) {
-        if ((not exists $l_instances{$_}) or ($l_instances{$_} == 0)) {
-            #$c_instances_modified{$cluster} = 0;
+        if ((not exists $l_instances{$_}) or ($l_instances_modified{$_} == 0)) {
             $bool = 0;
             last;
         } else {
             $l_instances_modified{$_} -= 1;
         }
     }
-    #if (%c_instances_modified{$cluster} == 0) {
     if ($bool == 0) {
-        #return (\%c_instances_modified, \%l_instances);
+        #print "bool in countdown_letters = $bool\n";
         return ($bool, \%l_instances);
     } else {
-        #$c_instances{$cluster} -= 1;
-        #return (\%c_instances, \%l_instances_modified);
+        #print "l_instances_modified in countdown_letters:\t";
+        #print "$_: $l_instances_modified{$_}\t" for keys(%l_instances_modified);
+        #print "\n";
         return ($bool, \%l_instances_modified);
     }
 }
 
 
 my %occurrences = letter_count($word);
-print "occurrences: ";
-print "$_: $occurrences{$_} \t" for keys(%occurrences);
-print "\n";
+#print "occurrences: ";
+#print "$_: $occurrences{$_} \t" for keys(%occurrences);
+#print "\n";
 
-#my %syllable_count = ();
 my @syllable_markers = ();
 my $syllables = calc_syllables($word);
 print "*************************************************\n";
 print "Maximum number of syllables = $syllables\n";
-#$syllable_count{"|"} = $syllables;
 push(@syllable_markers, "|");
-#print "$_: $syllable_count{$_}\t" for keys(%syllable_count);
 print "syllable symbols = @syllable_markers";
 print "\n";
 
@@ -199,103 +193,79 @@ print "occurrences: ";
 print "$_: $occurrences{$_} \t" for keys(%occurrences);
 print "\n";
 
-#my %vowel_count = cluster_count(\@vowel_clusters, \%occurrences);
 my @vowels = cluster_count(\@vowel_clusters, \%occurrences);
 print "*************************************************\n";
 print "Vowels and vowel clusters in input word :\n";
-#print "$_: $vowel_count{$_}\t" for keys(%vowel_count);
-print "@vowels\n";
-print "\n";
+print "@vowels\n\n";
 
-#my %not_coda_count = cluster_count(\@not_coda_clusters, \%occurrences);
 my @not_coda = cluster_count(\@not_coda_clusters, \%occurrences);
 print "*************************************************\n";
 print "Not coda clusters in input word :\n";
-#print "$_: $not_coda_count{$_}\t" for keys(%not_coda_count);
-print "\n";
+print "@not_coda\n\n";
 
-#my %not_onset_count = cluster_count(\@not_onset, \%occurrences);
 my @not_onset = cluster_count(\@not_onset_clusters, \%occurrences);
 print "*************************************************\n";
 print "Not onset clusters in input word :\n";
-#print "$_: $not_onset_count{$_}\t" for keys(%not_onset_count);
-print "@not_onset\n"; 
-print "\n";
+print "@not_onset\n\n";
 
-#my %onset_coda_ambisyllabic_count = cluster_count(\@onset_coda_ambisyllabic, \%occurrences);
 my @onset_coda_ambisyllabic = cluster_count(\@onset_coda_ambisyllabic_clusters, \%occurrences);
 print "*************************************************\n";
 print "Onset coda and ambisyllabic clusters in input word :\n";
-#print "$_: $onset_coda_ambisyllabic{$_}\t" for keys(%onset_coda_ambisyllabic_count);
-print "@onset_coda_ambisyllabic\n"; 
-print "\n";
+print "@onset_coda_ambisyllabic\n\n";
 
-#my @vertices = (\%syllable_count, \%vowel_count, \%not_coda_count, \%onset_coda_ambisyllabic_count, \%not_onset_count);
 my @vertices = (\@syllable_markers, \@vowels, \@not_coda, \@onset_coda_ambisyllabic, \@not_onset);
-#print "vertices = @vertices\n";
-#print "vertices[0] = $vertices[0]\n";
-print "\@vertices = syllables, vowels, not_coda, onset_coda_ambisyllabic, not_onset\n";
+#print "\@vertices = syllables, vowels, not_coda, onset_coda_ambisyllabic, not_onset\n";
 #my %adjacency_lists = (0 => [1,2,3], 1 => [0,2,3,4], 2 => [1], 3 => [0,1], 4 => [0,1]);
 #print "adjacency lists:\n";
 #print "$_: @{$adjacency_lists{$_}}\t" for keys(%adjacency_lists);
 #print "\n";
 
 my @adjacency_matrix = qw/0 1 1 1 0 1 0 1 1 1 0 1 0 0 0 1 1 0 0 0 1 1 0 0 0/; # 5 rows of size 5
-#print "@adjacency_matrix\n";
-#my $len = scalar(@adjacency_matrix);
-#print "Length of adjacency_matrix = $len\n";
 
 my @anagrams = ();
 
 sub concatenate {
     my ($word_length, $anagram_length, $vertex, $anagram_ref, $letter_count_ref) = @_;
     my @anagram = @{$anagram_ref};
-    my %letter_count = %{$letter_count_ref};
     if ($anagram_length >= $word_length) {
         my $anagram = join('.', @anagram);
         push(@anagrams, $anagram);
-        print "$anagram\n";
         return;
     }
+    my %letter_count = %{$letter_count_ref};
     my $row_start = $vertex * 5;
-    #print "row_start = $row_start\n";
     for(my $i=0; $i<5; $i++) {
         if ($adjacency_matrix[$row_start + $i] == 1) {
-            #print "i = $i\n";
-            #my %vertex = %{$vertices[$i]};
-            my @vertex = @{$vertices[$i]};
-            #if (%vertex) {
-            if (@vertex) {
-                #foreach my $key (keys %vertex) {
-                for(@vertex) {
-                    #my ($bool, $downcount_ref) = countdown_letters($key, \%letter_count);
-                    my ($bool, $downcount_ref) = countdown_letters($_, \%letter_count);
-                    my %downcount = %{$downcount_ref};
-                    print "$_: $downcount{$_}\t" for keys(%downcount);
-                    print "\n";
-                    if ($bool) {
-                        #print "key = $key\n";
-                        #push(@anagram, $key);
-                        push(@anagram, $_);
-                        #if ($key != '|') {
-                        #if ($key =~ /[^\|]/)  {
-                        #if ($key =~ /[a-z]|ä|ö|ü/)  {
-                        if ($_ =~ /[a-z]|ä|ö|ü/)  {
-                            #$anagram_length += length($key);
-                            $anagram_length += length($_);
-                            #print "key = $key, anagram length = $anagram_length\n";
-                            print "cluster = $_, anagram length = $anagram_length\n";
-                        } #else {
-                        #  print "key not matched: $key\n";
-                        #}
-                        concatenate($word_length, $anagram_length, $i, \@anagram, \%downcount);
-                    } else {
-                        #print "Not enough letters for cluster $key!\n";
-                        print "Not enough letters for cluster $_!\n";
+            my @next_vertex = @{$vertices[$i]};
+            for(@next_vertex) {
+                if (($vertex == 0) and (scalar(@anagram) >= 2)) { # last item in anagram is syllable gap
+                    if ($anagram[-2] =~ /[a|e|i|o|u|ä|ö|ü]/) { # second last item is vowel
+                        if ($i == 1) { # current cluster is vowel
+                            #print "vowel | vowel at end of anagram\n";
+                            #print "@anagram\t\t$_\n";
+                            next;
+                        }
+                    } else { # second last item in anagram must be consonant (cannot be syllable gap)
+                        if ($i != 1) { # current cluster is consonant
+                            if (scalar(@anagram) == 2) {
+                                #print "cons | cons at start of anagram:\n";
+                                #print "@anagram\t\t$_\n";
+                                next;
+                            }
+                        }
                     }
+                }   
+                my ($bool, $downcount_ref) = countdown_letters($_, \%letter_count);
+                my %downcount = %{$downcount_ref};
+                if ($bool) {
+                    push(@anagram, $_);
+                    if ($_ =~ /[a-z]|ä|ö|ü/)  {
+                        $anagram_length += length($_);
+                    }
+                    concatenate($word_length, $anagram_length, $i, \@anagram, \%downcount);
+                    pop(@anagram);
+                    $anagram_length -= length($_);
                 }
-            } else {
-                print "No clusters in vertex $vertex\n";
             }
         }
     }
@@ -306,4 +276,13 @@ my @anagram = ();
 
 #print "length of input word = $length\n";
 concatenate($length, 0, 0, \@anagram, \%occurrences);
-print "Anagrams: @anagrams\n";
+print "Anagrams: \n";
+for(@anagrams){
+    print "$_\n";
+}
+my @readable_anagrams = ();
+for(@anagrams) {
+    $_ =~ tr/|.//d;
+    push(@readable_anagrams, $_);
+}
+print "readable anagrams:\n @readable_anagrams\n";
